237856a8e5866176bfbbad2e60c9fefbcb6fdd40
0mega28 <sakshamdam54321@gmail.com>
2021-02-13 13:05:24 +0530

    Added function memory_set in kernel/util.h

--- kernel/util.c
+++ kernel/util.c
@@ -1,9 +1,18 @@
+#include "util.h"
+
 void memory_copy(char *source, char *dest, int nbytes)
 {
     for (int i = 0; i < nbytes; i++)
     {
         dest[i] = source[i];
     }
+}
+
+void memory_set(u8 *dest, u8 val, u32 len)
+{
+    u8 *temp = (u8 *)dest;
+    for (; len != 0; len--)
+        *temp++ = val;
 }
 
 void int_to_ascii(int n, char str[])
@@ -30,6 +39,5 @@ void int_to_ascii(int n, char str[])
         char temp = str[start];
         str[start++] = str[end];
         str[end--] = temp;
-    } 
-
+    }
 }
