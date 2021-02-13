d958c5b61f943edfed210133264a841819686120
0mega28 <sakshamdam54321@gmail.com>
2021-02-13 19:48:31 +0530

    Added function to reverse,strlen for strings

--- kernel/util.c
+++ kernel/util.c
@@ -41,3 +41,20 @@ void int_to_ascii(int n, char str[])
         str[end--] = temp;
     }
 }
+
+void reverse(char s[])
+{
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+int strlen(char s[])
+{
+    int i = 0;
+    while(s[i] != '\0') i++;
+    return i;
+}
