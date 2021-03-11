df92a923b4fcf846ec58f10e80232fbe6c0b3662
0mega28 <sakshamdam54321@gmail.com>
2021-03-11 20:07:43 +0530

    Initial commit for ordered_array
    For now we have just added header file for ordered_array

---
+++ libc/ordered_array.h
@@ -0,0 +1,51 @@
+/* 
+	Ordered array data structure is used to maintain holes
+	in heap.
+
+	This file provide interface to ordered array and functions
+	to create ordered array and insert and remove elements 
+	from ordered array. 
+
+	The array will be insertion sorted.
+ */
+
+#ifndef ORDERED_ARRAY_H
+#define ORDERED_ARRAY_H
+
+#include <stdint.h>
+
+/* Returns 1 if first argument is less than second argument
+	else return 0
+ */
+typedef int8_t (*lessthan_assert_t)(void *, void *);
+
+typedef struct
+{
+	void **array;
+	uint32_t size;
+	uint32_t max_size;
+	lessthan_assert_t less_than;
+} ordered_array_t;
+
+/* A standard less than assert */
+int8_t standard_lessthan_assert(void *a, void *b);
+
+/* Create ordered array */
+ordered_array_t create_ordered_array(uint32_t max_size, lessthan_assert_t less_than);
+
+/* When we don't have kmalloc we place the ordered array (like create ordered array)*/
+ordered_array_t place_ordered_array(void *addr, uint32_t max_size, lessthan_assert_t lessthan);
+
+/* Insert element into ordered array */
+void insert_into_ordered_array(void *item, ordered_array_t *array);
+
+/* Remove element from ordered array */
+void remove_from_ordered_array(uint32_t index, ordered_array_t *array);
+
+/* Lookup the item at index i */
+void* lookup_ordered_array(uint32_t index, ordered_array_t *array);
+
+/* Destroy ordered array */
+void destroy_ordered_array(ordered_array_t *array);
+
+#endif
