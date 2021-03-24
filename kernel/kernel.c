2a44a6b4c5a847902ebab7584b7b61978841d126
0mega28 <sakshamdam54321@gmail.com>
2021-03-24 13:32:07 +0530

    Cleaned main function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -10,39 +10,8 @@ void kmain()
 	isr_install();
 	irq_install();
 
-	uint32_t a = kmalloc(8);
 	initialise_paging();
-	uint32_t b = kmalloc(8);
-	uint32_t c = kmalloc(8);
-	/* monitor_write("a: ");
-    monitor_write_hex(a);
-    monitor_write(", b: ");
-    monitor_write_hex(b);
-    monitor_write("\nc: ");
-    monitor_write_hex(c); */
-
-	char buffer[10];
-	buffer[0] = 0;
-	hex_to_ascii(a, buffer);
-	kprint(buffer);
-	kprint("\n");
-	buffer[0] = 0;
-	hex_to_ascii(b, buffer);
-	kprint(buffer);
-	kprint("\n");
-
-	buffer[0] = 0;
-	hex_to_ascii(c, buffer);
-	kprint(buffer);
-	kprint("\n");
-
-	kfree((void *)c);
-	kfree((void *)b);
-	uint32_t d = kmalloc(12);
-	buffer[0] = 0;
-	hex_to_ascii(d, buffer);
-	kprint(buffer);
-
+	
 	kprint("Type END to halt the CPU\nType CLEAR to clear screen\n> ");
 }
 
